00399095237e2bbe3ae1d4c582c878a6422c837e
Nick Burt <nbur4556@gmail.com>
2025-11-03 12:46:36 -0600

    create an entity class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class Entity
+{
+
+    private bool isFemale;
+
+    public Entity()
+    {
+        var rand = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(rand.Next());
+        }
+    }
+}
